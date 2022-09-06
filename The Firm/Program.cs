072a14222f119c07e329c550a1565a2daa9c170b
Tue Munch Madsen <tuepvm@gmail.com>
2022-09-06 12:13:13 +0200

    Changes

--- The Firm/Program.cs
+++ The Firm/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Sweeper sweeper = new Sweeper("Jacob", "Anderson", "2312313333");
-            Manager manager = new Manager("Joey","Joe","0230123230", "34234485");
-            ProjectManager pm = new ProjectManager("Bob", "jakobsen", "5127335346", "29349529", "EMAIL@something.dk");
+            Employee sweeper = new Sweeper("Jacob", "Anderson", "2312313333");
+            Employee manager = new Manager("Joey","Joe","0230123230", "34234485");
+            Employee pm = new ProjectManager("Bob", "jakobsen", "5127335346", "29349529", "EMAIL@something.dk");
 
             manager.GetProperties();
             pm.GetProperties();
